150978e740bcbc8598e5b98927a1c9ea5a5719fb
Paul Ledbetter <ledbetter.paul+github@gmail.com>
2015-02-21 00:31:38 +0000

    Enhance mock-io to support disconnect events.

--- test/mock-io.js
+++ test/mock-io.js
@@ -8,40 +8,81 @@ var socket = function () {
     
     var sideA = new EventEmitter();
     var sideB = new EventEmitter();
-    var sideAemit = sideA.emit;
-    var sideBemit = sideB.emit;
+    var sideAemit = _.bind(sideA.emit, sideA);
+    var sideBemit = _.bind(sideB.emit, sideB);
+    var connected = true;
+
+    var socketDisconnect = function () {
+        if (connected) { 
+            connected = false;
+            sideBemit.call(sideB, 'disconnect');
+            sideAemit.call(sideA, 'disconnect');
+        }
+    };
     
     _.extend(sideA, {
         emit: function () {
-            sideBemit.apply(sideB, arguments);
+            if (connected) { 
+                sideBemit.apply(sideB, arguments);
+            }
         }
+        , disconnect: socketDisconnect
     });
 
     _.extend(sideB, {
         emit: function () {
-            sideAemit.apply(sideA, arguments);
+            if (connected) {
+                sideAemit.apply(sideA, arguments);
+            }
         }
+        , disconnect: socketDisconnect
     });
 
     return {
         sideA: sideA
         , sideB: sideB
+        , disconnect: function (quiet) {
+            connected = false;
+            if (!quiet) {
+                sideAemit('disconnect');
+                sideBemit('disconnect');
+            }
+        }
+        , connect: function () {
+            connected = true;
+            sideA.emit('connect');
+            sideB.emit('connect');
+        }
     };
 };
 
-var listenEmitter = new EventEmitter();
 
-_.extend(exports, {
-    connect: function () {
-        var s = socket();
-        setTimeout(function () {
-            listenEmitter.emit('connect', s.sideA);
-            s.sideA.emit('connect');
-        }, 0);
-        return s.sideB;
-    }
-    , listen: function () {
-        return listenEmitter;
-    }
-    , socket: socket
-});
+module.exports = function (behavior) {
+    var sockets = [];
+    var listenEmitter = new EventEmitter();
+
+    return {
+        connect: function () {
+            var s = socket();
+            setTimeout(function () {
+                listenEmitter.emit('connect', s.sideA);
+                if (behavior) {
+                    behavior(s.sideA);
+                }
+                else {
+                    s.sideA.emit('connect');
+                }
+            }, 0);
+            return s.sideB;
+        }
+        , listen: function () {
+            return listenEmitter;
+        }
+        , socket: socket
+        , sockets: function () {
+            return sockets;
+        }
+    };
+};
+
+module.exports.socket = socket;
